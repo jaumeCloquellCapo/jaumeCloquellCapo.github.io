21b10380a403d29b1473c1e203d9f82f714226c7
Jaume cloquell capo <jaumecloquell@hotmail.com>
2021-05-05 19:09:46 +0200

    aaeaf48d

---
+++ _nuxt/static/1620234542/categories/third-party-integrations/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/categories/third-party-integrations", {data:[{}],fetch:[],mutations:[["set",{content:"Extend the functionality of Awake with third party integrations\n",isEmpty:false,excerpt:"",name:"Third Party Integrations",image:"https:\u002F\u002Fpicsum.photos\u002F1800\u002F1801",pageType:"category",slug:"third-party-integrations"}]]});
---
+++ _nuxt/static/1620234542/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620234542",layout:"default",error:null,state:{pageType:a,title:a,subtitle:a,featureImage:a,content:a,author:a,date:a},serverRendered:true,routePath:"\u002Fcontact",config:{}}}(""));
